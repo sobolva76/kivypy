ba6c443794058a367044e57b234baa3e36bb5d59
sobolva76 <psisobol@yandex.ru>
2024-03-29 16:12:27 +0300

    PageLayout, Accordion end Button

--- KivyTest/k_button.py
+++ KivyTest/k_button.py
@@ -2,6 +2,8 @@ from kivy.app import App
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.accordion import Accordion
 
 from kivy.config import Config
 Config.set('graphics', 'resizable', True)
@@ -9,8 +11,12 @@ Config.set('graphics', 'resizable', True)
 from kivy.clock import Clock
 from kivy.animation import Animation
 
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.label import Label
+
 
 class BaseLayout(BoxLayout):
+
     def say_Hello(self):
         print("Hello")
 
@@ -29,10 +35,22 @@ class MainWindow(BoxLayout):
         anim.start(tgt)
 
 
+class CircularButton(ButtonBehavior, Label):
+    pass
+
+
+class MyPageLayout(PageLayout):
+    pass
+
+
+class MyAccord(Accordion):
+    pass
+
+
 class K_buttonApp(App):
     def build(self):
-        #return BaseLayout()
-        return MainWindow()
+        return BaseLayout()
+       #return MainWindow()
      
 
 if __name__ == '__main__':
