81c02b13476c730f8146cf26e4b38fdee07ebd55
sobolva76 <psisobol@yandex.ru>
2024-04-12 16:31:27 +0300

    рефакторинг

--- slugrace/results.py
+++ slugrace/results.py
@@ -5,9 +5,22 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.config import Config
+from kivy.properties import NumericProperty, StringProperty
 
 Config.set('graphics', 'width', '1200')
 Config.set('graphics', 'height', '675')
+
+
+class Result(BoxLayout):
+    player_name = StringProperty('')
+    money_before = NumericProperty(0)
+    bet_amount = NumericProperty(0)
+    slug_name = StringProperty('')
+    result_info = StringProperty('')
+    gain_or_loss = NumericProperty(0)
+    current_money = NumericProperty(0)
+    odds = NumericProperty(0)
+
 
 class ResultsScreen(BoxLayout):
     pass
