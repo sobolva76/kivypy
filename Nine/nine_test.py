22c8015828b631314b688d751d0d6644386a76e8
sobolva76 <psisobol@yandex.ru>
2024-05-15 16:31:36 +0300

    работа с коллизиями

--- Nine/nine_test.py
+++ Nine/nine_test.py
@@ -36,23 +36,38 @@ class Player(Widget):
         #print("Collide")
         if self.collide_widget(blok):
             if names == 'blok_right':
-                self.pos = (200, 200)
-                print("BAX-player", "blok_right")
+                #self.pos = (200, 200)
+                self.velocity_x *= -1
+                print("BAX-player", "blok_right", self.velocity)
+
             if names == 'blok_left':
-                self.pos = (200, 200)
-                print("BAX-player", "blok_left")
+                #self.pos = (200, 200)
+                self.velocity_x *= -1
+                print("BAX-player", "blok_left", self.velocity)
+                
+            if names == 'blok_bottom' and self.velocity_y != 0:
+                #self.pos = (200, 200)
+                self.velocity = (2, 0)
+                print("BAX-player", "blok_bottom", self.velocity)
+
+        else:
+            self.velocity = (0, -2)
+            print("нет коллизий")
 
 
 class NineTest(Widget):
     player = ObjectProperty(None)
     blok = ObjectProperty(None)
-    #blok_height = ObjectProperty(None)
     blok_base = ObjectProperty(None)
+    blok_down = ObjectProperty(None)
+    
 
     blok_right = 'blok_right'
     blok_left = 'blok_left'
+    blok_bottom = 'blok_bottom'
+    
 
-    def serve_player(self, vel=(2, 0)):
+    def serve_player(self, vel=(0, -2)):
         #self.player.center = self.center
         self.player.velocity = vel
 
@@ -64,6 +79,7 @@ class NineTest(Widget):
         #self.blok_height.blok_collide(self.player) 
         self.player.player_collide(self.blok, self.blok_right)
         self.player.player_collide(self.blok_base, self.blok_left)
+        self.player.player_collide(self.blok_down, self.blok_bottom)
 
 
 class Nine_testApp(App):
