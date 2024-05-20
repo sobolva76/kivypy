2ec33a46b4ef4a4966abaaa04dbdfb95e04eccc5
sobolva76 <psisobol@yandex.ru>
2024-05-20 16:34:55 +0300

    работа с циклами

--- Nine/nine_test3.py
+++ Nine/nine_test3.py
@@ -32,13 +32,20 @@ class NineTest(Widget):
     blok = ObjectProperty(None)
     blok_base = ObjectProperty(None)
     blok_down = ObjectProperty(None)
-    blok_down2 = ObjectProperty(None)
     blok_pol = ObjectProperty(None)
+
+    blok_down2 = ObjectProperty(None)
     stop_label = ObjectProperty(None)
+
+    blok_test = ObjectProperty(None)
+    blok_test2 = ObjectProperty(None)
+    blok_list = []
 
     collision_list = DictProperty({})
 
     players_list = []
+
+    rig_test = 1
     
 
     def serve_player(self, vel=(0, -2)):
@@ -47,6 +54,36 @@ class NineTest(Widget):
         self.player2.velocity = vel
         self.players_list = [self.player, self.player2]
 
+        blok = Blok(size = (10, 900), pos = (700, 0))
+        #self.blok_list = [blok_test]
+        self.blok_list.append(blok) 
+        self.add_widget(blok)
+
+        blok_base = Blok(size = (10, self.height), pos = (0, 0))
+        #self.blok_list = [blok_test]
+        self.blok_list.append(blok_base) 
+        self.add_widget(blok_base)
+
+        blok_down = Blok(size = (950, 10), pos = (100, 300))
+        #self.blok_list = [blok_test]
+        self.blok_list.append(blok_down) 
+        self.add_widget(blok_down)
+
+        blok_test = Blok(size = (200, 10), pos = (10, 150))
+        #self.blok_list = [blok_test]
+        self.blok_list.append(blok_test) 
+        self.add_widget(blok_test)
+
+        blok_test2 = Blok(size = (200, 10), pos = (100, 80))
+        self.blok_list.append(blok_test2)
+        self.add_widget(blok_test2)
+        #print("blok_list - ", self.blok_list)
+
+        blok_pol = Blok(size = (self.width, 10), pos = (0, 0))
+        self.blok_list.append(blok_pol)
+        self.add_widget(blok_pol)
+        #print("blok_list - ", self.blok_list)
+
 
     def update(self, dt):
 
@@ -54,35 +91,79 @@ class NineTest(Widget):
 
             i.move()
 
+            for blok_l in self.blok_list:# проходим по списку блоков
+
             #print(self.players_list)
 
-            if i.collide_widget(self.blok):
-                i.velocity_x *= -1
-                print("Collide blok")
+                if i.collide_widget(blok_l):
+                    #if blok_l == self.blok or blok_l == self.blok_base:
+                        #i.velocity_x *= -1
+                        #print("Collide blok, blok_base")
 
-            elif i.collide_widget(self.blok_base):
-                i.velocity_x *= -1
-                print("Collide blok_base")
-            
-            elif i.collide_widget(self.blok_pol) and i.velocity_y != 0:
-                i.velocity = (2, 0)
-                print("Collide blok_pol")
+                #elif i.collide_widget(self.blok_base):
+                    #i.velocity_x *= -1
+                    #print("Collide blok_base")
+                
+                #elif i.collide_widget(self.blok_pol) and i.velocity_y != 0:
+                    #i.velocity = (2, 0)
+                    #print("Collide blok_pol")
 
-            elif i.collide_widget(self.blok_down):
-                if self.collision_list.get(i) != self.blok_down:
-                    self.collision_list[i] = self.blok_down
-                    i.velocity = (2, 0)
-                    print("Collide blok_down", self.collision_list)
+                    #elif blok_l == self.blok_pol and i.velocity_y != 0:
+                        #i.velocity = (2, 0)
+                        #print("Collide blok_pol")
 
-            elif i.collide_widget(self.blok_down2):
-                i.velocity = (2, 0)
-                print("Collide blok_down2")
+                #elif i.collide_widget(self.blok_down):
+                    #if self.collision_list.get(i) != self.blok_down:
+                        #self.collision_list[i] = self.blok_down
+                        #i.velocity = (2, 0)
+                        #print("Collide blok_down", self.collision_list)
 
-            elif i.collide_widget(self.blok_pol) == False:
-                if self.collision_list.get(i):
-                    self.collision_list.pop(i)
-                    i.velocity = (0, -2)
-                    print("Collide NO", self.collision_list)
+                #elif i.collide_widget(self.blok_down2):
+                    #if self.collision_list.get(i) != self.blok_down2:
+                        #self.collision_list[i] = self.blok_down2
+                        #i.velocity = (2, 0)
+                        #print("Collide blok_down2")
+
+                #elif i.collide_widget(blok_l):
+                    #if self.collision_list.get(i) != blok_l:
+                        #self.collision_list[i] = blok_l
+                        #i.velocity = (2, 0)
+                        #print("Collide blok_test", self.collision_list)
+
+                    if self.collision_list.get(i) != blok_l:
+                    
+                        self.collision_list[i] = blok_l
+
+                        if self.rig_test == 1:
+                            i.velocity_x *= -1
+                            print("Collide blok, blok_base")
+
+                        elif blok_l == self.blok_pol and i.velocity_y != 0:
+                            i.velocity = (2, 0)
+                            print("Collide blok_pol")
+
+                        elif self.blok_pol == False:
+                            if self.collision_list.get(i):
+                                self.collision_list.pop(i)
+                                i.velocity = (0, -2)
+                                print("Collide NO")
+
+                        else:
+                            i.velocity = (2, 0)
+                            #print("Collide blok_test", self.collision_list)
+                            print(self.blok_base)
+
+                #elif i.collide_widget(self.blok_pol) == False:
+                    #if self.collision_list.get(i):
+                        #self.collision_list.pop(i)
+                        #i.velocity = (0, -2)
+                        #print("Collide NO", self.collision_list)  
+
+                    #elif self.blok_pol == False:
+                        #if self.collision_list.get(i):
+                            #self.collision_list.pop(i)
+                            #i.velocity = (0, -2)
+                            #print("Collide NO", self.collision_list)              
 
 
 class Nine_test3App(App):
