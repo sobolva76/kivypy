82d682ebcf9944b780bfd51c8c2feb26978b587a
sobolva76 <psisobol@yandex.ru>
2024-06-03 16:31:50 +0300

    blok_stop

--- Nine/nineGame.py
+++ Nine/nineGame.py
@@ -11,6 +11,11 @@ from kivy.clock import Clock
 class Blok(Widget):
     """ Класс блоков """
     pass
+class Blok_end_stop(Widget):
+    """ Класс блоков определяющих конец планки и конечного блока(при столкновении с ним - удаляется аппонент)"""
+    pass
+
+
 
 
 # класс противника в нем - скорость по осям, функция движения и имя
@@ -36,31 +41,42 @@ class NineGameBase(Widget):
     blok_left_name = "blok_left"
     blok_down = ObjectProperty(None)
     blok_down_name = "blok_down"
+    blok_stop = ObjectProperty(None)
+    blok_stop_name = "blok_stop"
 
     apponent_list = DictProperty({})# список противников (пустой)
     blok_list = DictProperty({})# список блоков
+    collision_list = DictProperty({})# список заполняется при первой коллизии аппонента с блоком
+
+    koll_apponents = 0 # колличество аппанентов, что-бы исключить бесконечное число
 
     def spawn_apponent(self, *args):
         """ функция спавна противников - создает и записывает в спивок"""
-        apponent = ObjectProperty(None)# создаем пустой объект противника
+        # определяем колличество аппонентов на уровне
+        if self.koll_apponents < 3:
+            apponent = ObjectProperty(None)# создаем пустой объект противника
 
-        # создаем противника и добавляем его на экран, и придаем ускорение
-        apponent = Apponent(pos = (50, 300))
-        self.add_widget(apponent)
-        apponent.vel_apponent = (2, 0)
+            # создаем противника и добавляем его на экран, и придаем ускорение
+            apponent = Apponent(pos = (100, 300))
+            self.add_widget(apponent)
+            apponent.vel_apponent = (0, -2)
 
-        self.apponent_list.update({apponent: apponent.name_apponent})# записываем аппонента в список
+            self.apponent_list.update({apponent: apponent.name_apponent})# записываем аппонента в список
+            self.koll_apponents += 1# - увеличиваем счетчик на 1
+            print(self.koll_apponents)
 
 
     def serve_objects(self):
         """ функция инициализации объектов"""
 
         # инициализация блоков
+        # инициализируем пол и стены
         self.blok_list.update({self.blok_right: self.blok_right_name})
         self.blok_list.update({self.blok_left: self.blok_left_name})
         self.blok_list.update({self.blok_down: self.blok_down_name})
+        self.blok_list.update({self.blok_stop: self.blok_stop_name})
 
-        print(self.blok_list)
+        #print(self.blok_list)
 
 
     def update(self, dt):
@@ -75,19 +91,32 @@ class NineGameBase(Widget):
                 if apponents.collide_widget(bloks):
                     # проверка на столкновение со стенами
                     if self.blok_list.get(bloks) == 'blok_right' or self.blok_list.get(bloks) == 'blok_left':
-                        #apponents.velocity_x *= -1
-                        print(apponents)
+                        # меняем скорость на противоположную
+                        apponents.vel_apponent_x *= -1
 
-                    if self.blok_list.get(bloks) == 'blok_down':
-                        apponents.velocity_x = 2
-                        print("collide down")
+                    # проверяем на столкновение с полом и записываем в список коллизий
+                    # что-бы исключить повторные столкновения
+                    if self.blok_list.get(bloks) == 'blok_down' and self.collision_list.get(apponents) != bloks:
+                        # если небыло коллизий - записываем в список и меняем скорость
+                        self.collision_list[apponents] = bloks
+                        apponents.vel_apponent = (2, 0)
+
+                    if self.blok_list.get(bloks) == 'blok_stop':
+                        # если небыло коллизий - записываем в список и меняем скорость
+                        apponents.vel_apponent_x *= -1
+                        self.remove_widget(apponents)
+                        self.collision_list.pop(apponents)
+                        #self.collision_list[apponents] = bloks
+                        #if self.koll_apponents < 3:
+                        self.koll_apponents = self.koll_apponents - 1
+                        print(self.collision_list)
 
 
 # класс запуска приложения
 class NineGame(App):
     def build(self):
         game = NineGameBase()
-        Clock.schedule_interval(game.spawn_apponent, 1 / 2) # 2 раза в секунду
+        Clock.schedule_interval(game.spawn_apponent, 3 / 1) # в н сикунд / н раз
         game.serve_objects()
         Clock.schedule_interval(game.update, 1 / 60.0)
         return game
