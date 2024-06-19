1eb78a3230a71d6e334731bb8e5680be40dc0c09
sobolva76 <psisobol@yandex.ru>
2024-06-19 16:33:07 +0300

    перенос этажи на другие файлы

---
+++ Nine/nineLevels/lev_1/levels_1.py
@@ -0,0 +1,32 @@
+# file levels_1.py - первый уровень
+import kivy
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, DictProperty
+from kivy.lang import Builder
+
+#Builder.load_file('levels_1.kv')
+
+
+class Blok_base(Widget):
+    """ класс блоков расположенных на уровне (не включая пол и стены) """
+    blok_base_name = "blok_base"
+
+class Blok_end(Widget):
+    """ Класс перехода между этажами """
+    blok_end_name = "blok_end"
+    
+
+blok_list_levels = DictProperty({})# список блоков
+
+def spawn_blok(self):
+    
+
+    blok_base = Blok_base(size = (100, 10), pos = (550, 150))
+    blok_list_levels = {blok_base: blok_base.blok_base_name}
+    self.add_widget(blok_base)
+    
+    blok_end = Blok_end(pos = (650, 155))
+    blok_list_levels = {blok_end: blok_end.blok_end_name}
+    self.add_widget(blok_end)
+
+    return blok_list_levels
